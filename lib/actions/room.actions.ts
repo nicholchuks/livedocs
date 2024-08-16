116fe300c7b99626bd7cf70341605b25de67acbc
Nicholas chuks <niccobery91@gmail.com>
2024-08-16 07:56:44 +0100

    first commit

---
+++ lib/actions/room.actions.ts
@@ -0,0 +1,37 @@
+"use server";
+
+import { nanoid } from "nanoid";
+import { liveblocks } from "../liveblocks";
+import { revalidatePath } from "next/cache";
+import { parseStringify } from "../utils";
+
+export const createDocument = async ({
+  userId,
+  email,
+}: createDocumentParams) => {
+  const roomId = nanoid();
+
+  try {
+    const metadata = {
+      createId: userId,
+      email,
+      title: "Untitled",
+    };
+
+    const usersAccesses: RoomAccesses = {
+      [email]: ["room:write"],
+    };
+
+    const room = await liveblocks.createRoom("my-room-id", {
+      metadata,
+      usersAccesses,
+      defaultAccesses: [],
+    });
+
+    revalidatePath("/");
+
+    return parseStringify(room);
+  } catch (error) {
+    console.log("Error happened while creating a room: ${error}");
+  }
+};
